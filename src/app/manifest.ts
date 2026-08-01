a7a115ce8100eca44d85d7a0989767da1e70d1fb
NextZone Admin <admin@nextzone.com>
2026-08-01 22:02:51 +0700

    Update PWA short_name to XFlix-Movies

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next';
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'XFlix - Watch Free Movies & TV Shows',
-    short_name: 'XFlix Movies',
+    short_name: 'XFlix-Movies',
     description: 'Watch unlimited movies and TV shows online free in HD, 1080p, 4K quality.',
     start_url: '/',
     display: 'standalone',
