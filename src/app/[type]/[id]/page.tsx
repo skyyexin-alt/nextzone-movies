0b97a44b19ca6afb2a27c70df98678f95bfe3752
NextZone Admin <admin@nextzone.com>
2026-08-05 17:43:48 +0700

    Remove Adsterra ad code

--- src/app/[type]/[id]/page.tsx
+++ src/app/[type]/[id]/page.tsx
@@ -8,7 +8,6 @@ import CastCarousel from '@/components/ui/CastCarousel';
 import IntegratedPlayer from '@/components/ui/IntegratedPlayer';
 import HistoryTracker from '@/components/ui/HistoryTracker';
 import GlobalBackButton from '@/components/ui/GlobalBackButton';
-import AdsterraBannerAd from '@/components/ui/AdsterraBannerAd';
 import AdskeeperBannerAd from '@/components/ui/AdskeeperBannerAd';
 export default async function DetailPage({
   params,
@@ -85,8 +84,6 @@ export default async function DetailPage({
         <span className="text-white truncate">{title}</span>
       </div>
 
-      {/* Adsterra Banner Ad above Player */}
-      <AdsterraBannerAd adKey="282e852f5808b9dd01d12c1ed30bf5d2" />
       {/* AdsKeeper Banner Ad */}
       <AdskeeperBannerAd widgetId="2064406" />
 
